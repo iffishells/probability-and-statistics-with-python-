b7695ae794072e5f2c79ab9b10bf9b2a38df2f3a
iffishells <iffishells@gmail.com>
2020-07-20 15:34:13 +0500

    added matplotlib working and data visualization

---
+++ start/stat-env/lib/xml2Conf.sh
@@ -0,0 +1,8 @@
+#
+# Configuration file for using the XML library in GNOME applications
+#
+XML2_LIBDIR="-L/home/ifti/Documents/Semester-5/prop-stat/start/stat-env/lib"
+XML2_LIBS="-lxml2 -L/home/ifti/Documents/Semester-5/prop-stat/start/stat-env/lib -lz -L/home/ifti/Documents/Semester-5/prop-stat/start/stat-env/lib -llzma -lpthread -L/home/ifti/Documents/Semester-5/prop-stat/start/stat-env/lib -L/home/ifti/Documents/Semester-5/prop-stat/start/stat-env/lib -licui18n -licuuc -licudata -lm "
+XML2_INCLUDEDIR="-I/home/ifti/Documents/Semester-5/prop-stat/start/stat-env/include/libxml2 -I/home/ifti/Documents/Semester-5/prop-stat/start/stat-env/include"
+MODULE_VERSION="xml2-2.9.10"
+
